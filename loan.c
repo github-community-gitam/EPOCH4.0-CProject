732bd8227fd90f6fa6cc49bfc8cd29f33d4a7ef4
Muskan <muskansngh15@gmail.com>
2025-12-24 10:31:13 +0530

    added problem statement for loan.c

--- loan.c
+++ loan.c
@@ -1,1 +1,7 @@
+/*An employee can apply for a loan at the beginning of every six months, but he/she will be sanctioned the amount according to the following company rules: 
 
+- Rule 1 : An employee cannot enjoy more than two loans at any point of time.
+- Rule 2 : Maximum permissible total loan is limited to Rs.50,000
+- Rule 3 : If there is any previous dues pending that amount would be deducted and the balance only will be given as loan.
+
+Write a program to read the values of the previous two loans got and process the new loan application and sanction the loan according to the eligibility.*/
